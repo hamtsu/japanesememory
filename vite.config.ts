3897df2dc6ba64187f7443f94e2b3fd314e18966
Hammy <68724706+hamtsu@users.noreply.github.com>
2024-05-29 11:53:01 +1000

    Update vite config and deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-  // base: "/japanesememory/"
+  base: "/japanesememory/",
   build: {
     outDir: 'dist'
   }
